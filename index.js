b5b369083d3dfa45c914aa5d63478179ce25aca8
soojeed <soojeed220@gmail.com>
2026-01-20 11:22:57 +0300

    Add All CRUD Successfully

---
+++ index.js
@@ -0,0 +1,13 @@
+var selectedRow = null;
+
+function showAlert(message, className){
+   const div = document.createElement("div");
+   div.className = `alert${className}`;
+
+   div.appendChild(document.createTextNode(message));
+   const container = document.querySelector(".container");
+   const main = document.querySelector(".main");
+   container.insertBefore(div, main);
+
+   setTimeout(() => document.querySelector(".alert").remove(), 3000)
+}
